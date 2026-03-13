4fd0810151d241d76c7dfc97003bd295ddf3d2d5
frisk-V3 <asahiharuto@outlook.jp>
2026-03-13 19:10:14 +0900

    Update vite.config.ts

--- ClientApp/vite.config.ts
+++ ClientApp/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: '../wwwroot',
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsDir: './'
   }
 });
